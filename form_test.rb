35c9a5111640fe6899c7969466d03af785c7e704
Kevin Emery <kevin.emery.dev@gmail.com>
2016-07-06 12:38:15 -0400

    proceeding with actual tests

--- form_test.rb
+++ form_test.rb
@@ -4,6 +4,7 @@ class FormTest < Test::Unit::TestCase
   def setup
     @driver = Selenium::WebDriver.for :firefox
     @test_url = "https://docs.google.com/forms/d/181whJlBduFo5qtDbxkBDWHjNQML5RutvHWOCjEFWswY"
+    @driver.get @test_url
   end
 
   def teardown
@@ -12,11 +13,23 @@ class FormTest < Test::Unit::TestCase
   end
 
   def all_fields_displayed
-    @driver.get @test_url
-    assert true
+    basic_form_xpaths.each_value { |this_xp| assert(@driver.find_element(:xpath => this_xp).displayed?, "could" \
+                                                    " not find an expected form element with xpath #{this_xp}") }
   end
 
   def required_field_test
-    assert true
+    @driver.find_element(:xpath => basic_form_xpaths[:submit]).click
+    sleep 50
+  end
+
+  def basic_form_xpaths
+    xpaths = Hash.new
+    xpaths[:name_field] = "//input[contains(@aria-label,'What is your name?')]"
+    xpaths[:enjoy_development_yes] = "//*[contains(@aria-label,'Do you enjoy development?')]/descendant::*[contains(.,'Yes')]/preceding-sibling::*/input[@type='checkbox']"
+    xpaths[:enjoy_development_no] = "//*[contains(@aria-label,'Do you enjoy development?')]/descendant::*[contains(.,'No')]/preceding-sibling::*/input[@type='checkbox']"
+    xpaths[:favorite_testing_framework] = "//select[contains(@aria-label,'What is your favorite testing framework?')]"
+    xpaths[:comments] = "//textarea[contains(@aria-label,'Comments?')]"
+    xpaths[:submit] = "//input[@type='submit']"
+    xpaths
   end
 end
